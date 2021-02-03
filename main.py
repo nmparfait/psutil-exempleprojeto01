202d8203270330f92e2ad79b466052d29708f5a1
nmparfait <mbamu.developer@gmail.com>
2021-02-03 20:40:01 +0000

    printing computer and network information with psutil

--- main.py
+++ main.py
@@ -32,3 +32,22 @@ for i in range(0, 10):
   print(psutil.cpu_percent())
   time.sleep(1)
 
+#conhecendo o disco 
+
+disco = psutil.disk_usage('.')
+
+print("Total:", disco.total, "B")
+print("Em uso:", disco.used, "B")
+print("Livre:", disco.free, "B")
+
+print("Total:", round(disco.total/(1024*1024*1024),2),"GB" )
+print("Em uso:", round(disco.used/(1024*1024*1024),2),"GB")
+print("Livre:", round(disco.free/(1024*1024*1024),2),"GB")
+
+print("Percentual de disco usado: ", disco.percent)
+
+#Capturando informações básicas da rede. 
+
+dic_interfaces = psutil.net_if_addrs()
+print(dic_interfaces)
+
